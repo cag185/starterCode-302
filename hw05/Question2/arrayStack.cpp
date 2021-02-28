5b8e75fcd1be9f6c23fa3ee2cf9959be9a98c351
cag185@pitt.edu <cag185@pitt.edu>
2021-02-28 08:12:43 -0500

    Most files implemented

---
+++ hw05/Question2/arrayStack.cpp
@@ -0,0 +1,143 @@
+//implementation file for stack
+//caleb gibson
+
+#include "arrayStack.hpp"
+
+//constructor
+template <typename T>
+arrayStack<T>::arrayStack()
+{
+    //set size = 1
+    stackLength = 1;
+    arrPoint = new T [stackLength]; //create a dynamically allocated array of inital size
+}
+
+//destructor
+template <typename T>
+~arrayStack<T>::arrayStack()
+{
+    //free memory
+    delete [] arrPoint;
+}
+
+//push
+template <typename T>
+void arrayStack<T>::push(T &val)
+{
+    //check if array needs to be resized
+    resize(); //automatically checks to see if the array needs to be resized and does it
+    //insert item at the last spot in array
+    arrPoint[usedSpace()] = val;
+    usedSpace++;
+}
+
+//pop -- returns item to show what was removed
+template <typename T>
+T arrayStack<T>::pop()
+{
+    resize(); //check for resize
+    T tempPlaceholder = arrPoint[usedSpace()];
+    //need to create a temp pointer, copy all values minus 1
+    T * tempPoint = new T [usedSpace-1]; 
+    for(std::size_t i = 0; i <usedSpace; i++)
+    {
+        tempPoint[i] = arrPoint[i];
+    }
+    delete [] arrPoint; //frees memory so we can reassign values
+    for(std::size_t j = 0; j <= stackSize; j++) // up to stack size to retain stack size
+    {
+        arrPoint[j] = tempPoint[j];
+    }
+    //free temp
+    delete [] tempPoint;
+    
+    //decrease size
+    usedSpace--;
+    return tempPlaceholder;
+}
+
+//peek -- returns item but does not remove
+template <typename T>
+T arrayStack<T>::peek()
+{
+
+}
+
+//is empty
+template <typename T>
+bool arrayStack<T>::isEmpty()
+{
+    if(usedSpace == 0)
+    {
+        return true;
+    }
+  return false;
+}
+
+//return size 
+template <typename T>
+std::size_t arrayStack<T>::size()
+{
+    return stackLength;
+}
+
+//return the used space
+template <typename T>
+std::size_t arrayStack<T>::usedSpace()
+{
+    return usedSpace;
+}
+
+//resize function
+template <typename T>
+void arrayStack <T>:: resize()
+{
+    //check to see of the array needs to be resized
+    if(stackLength == usedSpace)
+    {
+        //array needs to double
+        //create a temp pointer
+        T *tempPointer = new [size() * 2]; //creates a temp
+        //pointer to an array twice the size of the original
+        for(std::size_t i = 0; i<=size(); i++)
+        {
+            //assign all values from old pointer to new
+            tempPointer[i] = arrPoint[i];
+        }
+        //delete old pointer
+        delete []  arrPoint;
+        //recopy values into new arrPoint
+        stackLength = 2 * stackLength; // increase size of stack
+        arrPoint = new T[stackLength];
+        for(std::size_t j = 0; j <= size(); j++)
+        {
+            arrPoint[j] = tempPointer[j]; //copy into resized array
+        }
+        //free memory
+        delete [] tempPointer;
+
+        if(usedSpace <stackLength/2 && stackLength > 1)
+        {
+            //do the same thing but now halve the array
+            T *tempPoint = new [size() / 2];
+            for(std::size_t x = 0; x <= size()/2; x++)
+            {
+                tempPoint[x] = arrPoint[x];
+            }
+            //free memory
+            //change size of stack
+            stackLength = stackLength / 2;
+            delete [] arrPoint;
+            //remake arrPoint
+            arrPoint = new T [stackLength];
+            //copy all values back
+            for(std::size_t y = 0; y <= stackLength; y++)
+            {
+                arrPoint[y] = tempPoint[y];
+            }
+            //free temp Point
+            delete [] tempPoint;
+        }
+
+    }
+}
---
+++ hw05/Question2/arrayStack.hpp
@@ -0,0 +1,44 @@
+//use an array to create a stack implementation
+//Caleb Gibson
+
+template <typename T>
+class arrayStack{
+    //variables here
+    private:
+        //use an array
+        //create pointer to type T arrayStack
+        T *arrPoint;
+        //size variable
+        std::size_t stackLength;
+        std::size_t usedSpace;
+    public:
+        //anytime stack becomes full, double the array
+        //stacks bottom entry is the beggining of array
+
+        //constructor
+        arrayStack();
+
+        //destructor
+        ~arrayStack();
+
+        //push function
+        void push(T &val);
+
+        //pop function
+        T pop(T);
+
+        //peek function
+        T peek();
+
+        //is empty
+        bool isEmpty();
+
+        //size function
+        std::size_t size();
+
+        //return used space
+        std::size_t usedSpace();
+
+        //resize function
+        void resize();
+};
